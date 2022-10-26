2560b1c732d8f06a2b9fd4b5a1d93970521f6529
Nikola Nikolov <106319618+Niko5433@users.noreply.github.com>
2022-10-26 16:02:24 +0300

    Update

--- src/GameCode.java
+++ src/GameCode.java
@@ -6,48 +6,56 @@ public class GameCode {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("How many rounds would you like to play?");
+
+        System.out.println("What is your name?");
+        String playerName = scanner.nextLine();
+
+        System.out.printf("How many rounds do you want to play, %s ?%n", playerName);
         int rounds = Integer.parseInt(scanner.nextLine());
 
+
         for (int i = 0; i < rounds; i++) {
-            playRockPaperScissors(scanner);
-        }
-    }
+            System.out.printf("Choose your move, %s%n", playerName);
+            System.out.println("[1] Rock");
+            System.out.println("[2] Paper");
+            System.out.println("[3] Scissors");
 
-    static void playRockPaperScissors(Scanner scanner) {
-        //input from the user
-        System.out.println("Choose: Rock, Paper or Scissors: ");
-        String playerMove = scanner.nextLine();
+            String playerMove = scanner.nextLine();
+            int playerMoveNum = Integer.parseInt(playerMove);
+            if (playerMoveNum > 3){
+                System.out.println("Invalid move");
+                continue;
+            }
 
-        //random input from computer
-        Random random = new Random();
-        int randomNumber = random.nextInt(3);
+            Random random = new Random();
+            int randomNumber = random.nextInt(3);
 
-        String computerMove;
-        if (randomNumber == 0) {
-            computerMove = "Rock";
-        } else if (randomNumber == 1) {
-            computerMove = "Paper";
-        } else {
-            computerMove = "Scissors";
-        }
-        System.out.println("Computer chose: " + computerMove + "!");
+            String computerMove;
+            if (randomNumber == 0) {
+                computerMove = "Rock";
+            } else if (randomNumber == 1) {
+                computerMove = "Paper";
+            } else {
+                computerMove = "Scissors";
+            }
+            System.out.println("Computer chose: " + computerMove + "!");
 
-        //Print results
-        if (playerMove.equals(computerMove)) {
-            System.out.println("It's a draw!");
-        } else if (playerWins(playerMove, computerMove)) {
-            System.out.println("Player wins!");
-        } else {
-            System.out.println("Computer wins!");
+            if (playerMove.equals(computerMove)) {
+                System.out.println("It's a draw!");
+            } else if (playerWins(playerMove, computerMove)) {
+                System.out.println(playerName + " wins!");
+            } else {
+                System.out.println("Computer wins!");
+            }
+
         }
 
     }
 
     static boolean playerWins(String playerMove, String computerMove) {
-        if (playerMove.equals("Rock")) {
+        if (playerMove.equals("1")) {
             return computerMove.equals("Scissors");
-        } else if (playerMove.equals("Paper")) {
+        } else if (playerMove.equals("2")) {
             return computerMove.equals("Rock");
         } else {
             return computerMove.equals("Paper");
